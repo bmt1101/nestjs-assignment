25a93b4298ed05f186a1782b7bae89c0c046aaa9
bmt1101 <101403984+bmt1101@users.noreply.github.com>
2024-10-28 23:51:01 +0800

    Delete prime-number.module.ts

--- prime-number.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrimeNumberController } from './prime-number.controller';
-import { PrimeNumberService } from './prime-number.service';
-
-@Module({
-  controllers: [PrimeNumberController],
-  providers: [PrimeNumberService]
-})
-export class PrimeNumberModule {}
