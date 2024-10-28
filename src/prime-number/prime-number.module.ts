b6b6d5acad05e6b3f400cd76769ddfe296b08ad3
bmt1101 <blessietejas1101@gmail.com>
2024-10-28 23:53:20 +0800

    Completed Assignment 2: Prime Number

---
+++ src/prime-number/prime-number.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PrimeNumberController } from './prime-number.controller';
+import { PrimeNumberService } from './prime-number.service';
+
+@Module({
+  controllers: [PrimeNumberController],
+  providers: [PrimeNumberService]
+})
+export class PrimeNumberModule {}
